f9882c89b69b360e510dc1326224846f4195c693
Lakshya Shrivastava <69989952+LakshyaShrivastava@users.noreply.github.com>
2024-12-25 13:09:49 -0800

    Added visuals for AI speaking/ recognizing words. added start up audio

---
+++ engine/features.py
@@ -0,0 +1,5 @@
+from playsound import playsound
+
+
+def playStartsSound():
+    playsound("webUI\\assets\\audio\\start_sound.mp3")
--- webUI/main.js
+++ webUI/main.js
@@ -1,13 +1,38 @@
 $(document).ready(function () {
-    
+
     $('.text').textillate({
         loop: true,
         sync: true,
         in: {
             effect: "bounceIn"
         },
-        out:{
+        out: {
             effect: "bounceOut"
+        }
+    })
+
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+    });
+
+    // AI msg animation
+    $('.lisa-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true
         }
     })
 
